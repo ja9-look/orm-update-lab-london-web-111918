d9dd19075f69a83be302f10f25f146096cfd4a36
Janine Luk <janineluk95@gmail.com>
2018-11-29 15:28:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,34 @@
 require_relative "../config/environment.rb"
 
 class Student
+  
+  DB = { :conn => SQLite3::Database.new('./students.db')}
+  DB[:conn].results_as_hash = true
+  
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name 
+    @grade = grade
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT);
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  
 
 
 end
